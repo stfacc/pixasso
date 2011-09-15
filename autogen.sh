fc9ada1cdff0c8f5650715e05bc89d22ac3b7f40
Stefano Facchini <stefano.facchini@gmail.com>
2011-09-15 14:24:27 +0200

    build: use a proper autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+# Run this to generate all the initial makefiles, etc.
 
-echo "Generating files..."
-autoreconf --install || {
-  echo Error.
-  exit 1
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="pixasso"
+REQUIRED_AUTOMAKE_VERSION=1.7
+
+(test -f $srcdir/configure.ac \
+  && test -d $srcdir/src) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level pixasso directory"
+    exit 1
 }
 
-echo "done."
-echo "Running configure..."
-./configure "$@"
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME git"
+    exit 1
+}
+. gnome-autogen.sh
